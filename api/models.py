fdadef969bb9c2a67c0fa3fc652dca33eaec5dc0
Dmitrii Koshelev <me@koshelev.net>
2020-12-10 11:15:11 +0300

    Реализована модель категорий Category

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
+"""Contains models to provide an Object-relational Mapping in 'api' app."""
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Stores a single category entry.
+    """
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        null=True,
+        verbose_name='ЧПУ',
+    )
+
+    class Meta():
+        """Adds meta-information."""
+
+        verbose_name_plural = 'Категории произведений'
+        verbose_name = 'Категория'
+
+    def __str__(self):
+        """Return category info."""
+        return (f'Category "{self.name[:50]}", '
+                f'slug="{self.slug[:50]}"')
