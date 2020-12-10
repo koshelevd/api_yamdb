4dc140e9f2ae040ff6ef5ca410b7ebe13fa88f68
Dmitrii Koshelev <me@koshelev.net>
2020-12-10 11:42:40 +0300

    Реализована модель произведений Title

--- api/models.py
+++ api/models.py
@@ -56,3 +56,46 @@ class Genre(models.Model):
         """Return genre info."""
         return (f'Genre "{self.name[:50]}", '
                 f'slug="{self.slug[:50]}"')
+
+
+class Title(models.Model):
+    """
+    Stores a single title entry.
+    """
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    year = models.DecimalField(
+        max_digits=4,
+        null=True,
+        verbose_name='Год выпуска',
+    )
+    category = models.ForeignKey(
+        'Category',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Категория',
+    )
+    genres = models.ManyToManyField(
+        'Genre',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
+        related_name='titles',
+        verbose_name='Жанры',
+    )
+
+    class Meta():
+        """Adds meta-information."""
+
+        verbose_name_plural = 'Произведения'
+        verbose_name = 'Произведение'
+
+    def __str__(self):
+        """Return title's info."""
+        return (f'Title "{self.name[:50]}", '
+                f'year "{self.year}"')
