eb8ca72dec67076d7053515f682b09a7fe67c9c8
Dmitrii Koshelev <me@koshelev.net>
2020-12-10 11:17:54 +0300

    Реализована модель жанров Genre

--- api/models.py
+++ api/models.py
@@ -28,3 +28,31 @@ class Category(models.Model):
         """Return category info."""
         return (f'Category "{self.name[:50]}", '
                 f'slug="{self.slug[:50]}"')
+
+
+class Genre(models.Model):
+    """
+    Stores a single genre entry.
+    """
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Название',
+    )
+    slug = models.SlugField(
+        max_length=250,
+        unique=True,
+        null=True,
+        verbose_name='ЧПУ',
+    )
+
+    class Meta():
+        """Adds meta-information."""
+
+        verbose_name_plural = 'Жанры произведений'
+        verbose_name = 'Жанр'
+
+    def __str__(self):
+        """Return genre info."""
+        return (f'Genre "{self.name[:50]}", '
+                f'slug="{self.slug[:50]}"')
