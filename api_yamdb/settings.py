d64dd12fcefccf4df26ba8b5678b5bf2816d5681
Dmitrii Koshelev <me@koshelev.net>
2020-12-11 14:04:44 +0300

    Добавлено приложение rest_framework

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/'),)
+
