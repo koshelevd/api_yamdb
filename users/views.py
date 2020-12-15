13a820ecdaaf97479dc79dc0b758d35f8704b81b
Dmitrii Koshelev <me@koshelev.net>
2020-12-15 09:35:32 +0300

    YMDB-15: Add pagination to viewset

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 """View classes of the 'api' app."""
 from rest_framework import viewsets, mixins
+from rest_framework.pagination import PageNumberPagination
 
 from .models import YamdbUser
 from .serializers import UserSerializer
@@ -9,5 +10,6 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     Viewset for 'users.models.YamdbUser' model.
     """
-
+    queryset = YamdbUser.objects.all()
     serializer_class = UserSerializer
+    pagination_class = PageNumberPagination
